911b9c5e81a159e8a43195dbb5ae69f686615ebb
yassineAbou <aboyassine555@gmail.com>
2023-09-09 16:27:55 +0100

    chore: Disable supporting right to left languages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.yassineabou.clock"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
